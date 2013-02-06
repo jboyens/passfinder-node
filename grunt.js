de0e608c9b08ccb71efa08151d0e459acea4b679
JR Boyens <jboyens@fooninja.org>
2013-02-06 00:13:14 -0800

    Grunt tasks and other sundry items

--- grunt.js
+++ grunt.js
@@ -1,14 +1,40 @@
 module.exports = function(grunt) {
+  "use strict";
+
   grunt.initConfig({
     jasmine_node: {
       specNameMatcher: "spec",
       projectRoot: ".",
       requirejs: false,
       forceExit: true
+    },
+    lint: {
+      all: ["grunt.js", "lib/*.js"]
+    },
+    jshint: {
+      options: {
+        browser: true,
+        curly: true,
+        eqeqeq: true,
+        newcap: true,
+        undef: true,
+        eqnull: true,
+        node: true,
+        strict: false
+      },
+      globals: {
+        exports: true,
+        Ext: false,
+        console: false,
+        alert: false,
+        prompt: false
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-jasmine-node');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-check-modules');
 
-  grunt.registerTask('default', 'jasmine_node');
-}
+  grunt.registerTask('default', ['lint','check-modules','jasmine_node']);
+};
--- spec/passfinderSpec.js
+++ spec/passfinderSpec.js
@@ -1,16 +1,25 @@
-var passfinder = require("../lib/passfinder");
+var Passfinder = require("../lib/passfinder");
 
 describe("Passfinder", function() {
-  var p;
+  var passfinder;
 
   beforeEach(function() {
-    p = new passfinder.Loader();
+    spyOn(Passfinder.prototype, '_load');
+
+    passfinder = new Passfinder();
   });
 
   it("should define a list of attractions and urls", function() {
-    expect(p.passes).toBeDefined();
+    expect(passfinder.passes).toBeDefined();
   });
 
   describe(".load", function() {
+    it("should do that thing", function() {
+      passfinder.load('CoitTower');
+
+      expect(passfinder._load).toHaveBeenCalledWith(jasmine.objectContaining({
+        CoitTower: "http://sflib1.sfpl.org/record=b2534123~S1"
+      }));
+    });
   });
 });
